db1753c105231917a012a874b8acf36b21fcd509
Hannes Leonhard <hannes.leonhard@tum.de>
2023-10-08 14:23:23 +0200

    IMplement dynamic snow overlay

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     ],
     packages=["perturbationdrive"],
     include_package_data=True,
-    install_requires=["numpy", "opencv-python", "scipy"],
+    install_requires=["numpy", "opencv-python", "scipy", "itertools"],
     python_requires='>=3'
 )
