391b99c74bb49f58f825266fb430ce4fe6f52740
Stefano Lambertenghi <stefanolambe@me.com>
2025-09-02 09:39:01 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,51 +1,13 @@
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+import platform
+import runpy
+import sys
 
-# To use a consistent encoding
-from codecs import open
-from os import path
+os_name = platform.system().lower()
 
-# The directory containing this file
-HERE = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(HERE, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
-
-# This call to setup() does all the work
-setup(
-    name="perturbationdrive",
-    version="0.0.1",
-    description="A library to test the robstuness of Self-Driving-Cars",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://",
-    author="Hannes Leonhard",
-    author_email="example@email.com",
-    license="MIT",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Operating System :: OS Independent",
-    ],
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        "numpy",
-        "opencv-python",
-        "scipy",
-        "matplotlib",
-        "scikit-image",
-        "requests",
-        "Pillow",
-        "tensorflow",
-        "tensorflow-addons",
-    ],
-    python_requires=">=3",
-)
+if os_name == 'linux':
+    runpy.run_path("setup_linux.py", run_name="__main__")
+elif os_name == 'darwin':
+    runpy.run_path("setup_macos.py", run_name="__main__")
+else:
+    print(f"Unsupported OS: {os_name}", file=sys.stderr)
+    sys.exit(1)
